dd3a30dd03ccd2ce02704f6f40ac3bb440cc21bf
mworzala <mattheworzala@gmail.com>
2023-11-30 23:38:27 +0200

    chore: publish on maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     `java-library`
+
+    `maven-publish`
+    signing
+    alias(libs.plugins.nexuspublish)
 }
 
-group = "net.hollowcube"
-version = "1.0-SNAPSHOT"
+group = "dev.hollowcube"
+version = System.getenv("TAG_VERSION") ?: "dev"
+description = "Schematic reader and writer for Minestom"
 
 repositories {
     mavenCentral()
@@ -19,6 +24,84 @@ dependencies {
     testImplementation(libs.bundles.logback)
 }
 
+java {
+    withSourcesJar()
+    withJavadocJar()
+
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+nexusPublishing {
+    this.packageGroup.set("dev.hollowcube")
+
+    repositories.sonatype {
+        nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+        snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+
+        if (System.getenv("SONATYPE_USERNAME") != null) {
+            username.set(System.getenv("SONATYPE_USERNAME"))
+            password.set(System.getenv("SONATYPE_PASSWORD"))
+        }
+    }
+}
+
+publishing.publications.create<MavenPublication>("maven") {
+    groupId = "dev.hollowcube"
+    artifactId = "schem"
+    version = project.version.toString()
+
+    from(project.components["java"])
+
+    pom {
+        name.set(artifactId)
+        description.set(project.description)
+        url.set("https://github.com/hollow-cube/schem")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://github.com/hollow-cube/schem/blob/main/LICENSE")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("mworzala")
+                name.set("Matt Worzala")
+                email.set("matt@hollowcube.dev")
+            }
+        }
+
+        issueManagement {
+            system.set("GitHub")
+            url.set("https://github.com/hollow-cube/schem/issues")
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/hollow-cube/schem.git")
+            developerConnection.set("scm:git:git@github.com:hollow-cube/schem.git")
+            url.set("https://github.com/hollow-cube/schem")
+            tag.set(System.getenv("TAG_VERSION") ?: "HEAD")
+        }
+
+        ciManagement {
+            system.set("Github Actions")
+            url.set("https://github.com/hollow-cube/schem/actions")
+        }
+    }
+}
+
+signing {
+    isRequired = System.getenv("CI") != null
+
+    val privateKey = System.getenv("GPG_PRIVATE_KEY")
+    val keyPassphrase = System.getenv()["GPG_PASSPHRASE"]
+    useInMemoryPgpKeys(privateKey, keyPassphrase)
+
+    sign(publishing.publications)
 }
